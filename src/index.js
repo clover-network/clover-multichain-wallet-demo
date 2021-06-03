79919db89e841e96b11b32e2419d527c51720ba8
Deyang Liu <deyangliu@DeyangdeMacBook-Pro.local>
2021-06-03 10:28:13 +0800

    Connect wallet with web3 react

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Web3ReactProvider } from '@web3-react/core'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './state';
+import { getLibrary } from './utils/web3ReactUtils'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Web3ReactProvider getLibrary={getLibrary}>
       <Provider store={store}>
-          <App />
+        <App />
       </Provider>
+    </Web3ReactProvider>  
   </React.StrictMode>,
   document.getElementById('root')
 );
